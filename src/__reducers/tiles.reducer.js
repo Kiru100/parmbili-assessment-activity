da03703a75bca5ec61fb0358c0971bc6d5fd2d8e
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-01-05 14:09:26 +0800

    Added documentation to reducers

--- src/__reducers/tiles.reducer.js
+++ src/__reducers/tiles.reducer.js
@@ -27,21 +27,24 @@ export const tiles = createSlice({
         ]
     },
     reducers:{
+        /* Changes tile mode to tilled. */
         setTillMode:(state, action) =>{
             const { tile_index } = action.payload;
             state.tiles_list[tile_index].mode = "tilled";
         },
+        /* Changes tile mode to empty then clear the interval if existing. */
         setEmptyMode:(state, action) =>{
             const { tile_index } = action.payload;
             clearInterval(state.tiles_list[tile_index].interval_id);
             state.tiles_list[tile_index] = { mode:"empty" };
         },
+        /* Changes tile mode to planted then set which crop is planted. */
         setPlantMode:(state, action) =>{
             const { tile_index, plant_name } = action.payload;
-
             state.tiles_list[tile_index].mode = "planted";
             state.tiles_list[tile_index].plant_name = plant_name;
         },
+        /* Changes tile mode to harvest if previous mode is planted. */
         setHarvestMode:(state, action)=>{
             const { tile_index } = action.payload;
         
@@ -49,15 +52,18 @@ export const tiles = createSlice({
                 state.tiles_list[tile_index].mode = "harvest";
             }
         },
+        /* Changes tile mode to harvest if previous mode is planted. */
         setPlantTimer:(state, action)=>{
             const {tile_index,time_left , interval_id} = action.payload;
             state.tiles_list[tile_index].time_left = time_left;
             state.tiles_list[tile_index].interval_id = interval_id;
         },
+        /* Changes the value of selected tile to current one given index and mode. */
         setSelectedTile:(state, action)=>{
             const {index, mode} = action.payload;
             state.selected_tile = {index: index, mode: mode};
         },
+        /* Add new sets of array to tiles_list. */
         expandTiles:(state, action)=>{
             const { to_add } = action.payload;
 
--- src/__reducers/users.reducer.js
+++ src/__reducers/users.reducer.js
@@ -6,14 +6,17 @@ export const users = createSlice({
         user_data:{ total_earnings: 50, level: 0 }
     },
     reducers:{
+        /* Increase total earnings of user. */
         addTotalEarnings:(state, action)=>{
             const { earning_value } = action.payload;
             state.user_data.total_earnings += earning_value;
         },
+        /* Decrease total earnings of user. */
         reduceTotalEarnings:(state, action)=>{
             const { expense_value } = action.payload;
             state.user_data.total_earnings -= expense_value;
         },
+        /* Increment user's level then decrease total earnings of user. */
         addUserLevel:(state, action)=>{
             const { expansion_price } = action.payload;
             state.user_data.level += 1;

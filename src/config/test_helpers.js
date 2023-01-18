46046c5a40d51daa11010eb984d036f156075ebf
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-01-18 13:08:13 +0800

    Create test_helpers.js

---
+++ src/config/test_helpers.js
@@ -0,0 +1,24 @@
+const TIMEOUT_SPEED = {
+    fastest: 200,
+    faster: 500,
+    normal: 1000,
+    slow: 2000,
+    slowest: 3000,
+    custom: 150000
+};
+
+const ASSERT_DURATION = {
+    fastest: 50,
+    faster: 2000,
+    default: 30000,
+    slowest: 61000
+};
+
+const SCREEN_SIZE = {
+    witdh: 1280,
+    height: 900
+};
+
+exports.TIMEOUT_SPEED = TIMEOUT_SPEED;
+exports.ASSERT_DURATION = ASSERT_DURATION;
+exports.SCREEN_SIZE = SCREEN_SIZE;

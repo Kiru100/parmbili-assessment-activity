6cbdd655c0677fb9af12582debc81a0757950975
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-01-18 10:03:30 +0800

    Create test_constants.js

---
+++ src/config/test_constants.js
@@ -0,0 +1,24 @@
+const TIMEOUT_SPEED = {
+    fastest: 200,
+    faster: 500,
+    normal: 1000,
+    slow: 2000,
+    slowest: 3000
+};
+
+const ASSERT_DURATION = {
+    fastest: 50,
+    faster: 2000,
+    default: 30000,
+    slowest: 61000
+}
+
+const AUTO_HARVEST = {
+    start_index: 0,
+    end_index: 2,
+    tile_order_number: 16
+}
+
+exports.TIMEOUT_SPEED = TIMEOUT_SPEED;
+exports.ASSERT_DURATION = ASSERT_DURATION;
+exports.AUTO_HARVEST = AUTO_HARVEST;
